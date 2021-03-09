01f611e561abf91e2e71b7022883584ca5848511
msaum <2564117+msaum@users.noreply.github.com>
2021-03-09 12:45:20 -0400

    - Add .gitignore - Create a README.md - mfa.sh:    - Add AWS CLI validation    - Add a config variable for credential duration seconds

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # aws-mfa
+
+## Overview
+This script will fetch an MFA token so that you can use temporary AWS credentials. This is modeled on several 
+other similar scripts on the Internet with a few add-ons.  It will remember the last credential generated, 
+so that if you open a new shell window or tab, you can use the last 
+credential generated when your shell startup script is run. 
+This script is intended to be added to a `.profile`, `.bashrc`, `.zshrc` or similar file.
+
+## Configuration
+
+- Copy the code from `mfa.sh` into your shell startup file.
+- Create entries in your `~/.aws/config` and `~/.aws/credentials` files for each profile you want to use similar to 
+  the following.  Your MFA ARN will be pulled from the `.aws/credentials` file automatically without any need for 
+  another configuration file.
+  
+### `~/.aws/config`
+```[default]
+cli_pager = less
+region = us-east-1
+output = json
+```
+
+### `~/.aws/credentials`
+```
+[default]
+aws_access_key_id = XXXXXXXXXXXXXXXXXXXXXXXX
+aws_secret_access_key = XXXXXXXXXXXXXXXXXXXXXXXX
+aws_mfa_device = arn:aws:iam::XXXXXXXXXXXXXXXXXXXXXXXX:mfa/USERID
+```
+**Note:** Ensure you are using the mfa ARN not the IAM user ARN.
+
+### Usage
+The function takes one or two arguments.  By default, you can just send the token-code (2FA code), 
+and it will use the default profile.  Optionally you can use an aws profile to pick a different profile from the
+`~/.aws/config` / `~/.aws/credentials` files.
+
+`Usage: mfa token-code [profile]`
+
+
+At a command prompt run the following command: 
+
+`# mfa <mfacode> <optional-aws-profile>`
+
+# Implementation Note
+
+In using this function/script the temporary credentials generated will persist across shells that execute
+the shell profile through the use of the cache file.  This cache file is stored in `~/.aws/.credcache`.
--- mfa.sh
+++ mfa.sh
@@ -10,35 +10,44 @@ if [ -f "${AWS_CREDS_CACHE}" ]; then
 fi
 
 function mfa {
-		 # Zero out existing credentials
-     unset AWS_ACCESS_KEY_ID
-     unset AWS_SECRET_ACCESS_KEY
-     unset AWS_SESSION_TOKEN
+  AWS_CLI=`which aws`
+  if [ $? -ne 0 ]; then
+    echo "AWS CLI is not installed.  Please install AWS CLI v2"
+    echo "https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html"
+    return 1
+  fi
+  # Creds persist for 36 hours (129600)
+  DURATION_SECONDS=129600
 
-     # make sure two arguments are passed else die
-     [ $# -eq 0 ] && { echo "Usage: $0 token-code [profile]" ; return 1 }
-     [ $# -gt 3 ] && { echo "Usage: $0 token-code [profile]" ; return 1 }
+  # Zero out existing credentials
+  unset AWS_ACCESS_KEY_ID
+  unset AWS_SECRET_ACCESS_KEY
+  unset AWS_SESSION_TOKEN
 
-     [ $# -eq 1 ] && { \
-     	  ARN_OF_MFA=`aws configure get aws_mfa_device --profile default` && \
-     	  TOKEN_CODE=$1 && \
-     	  echo "Running: aws sts get-session-token --duration-seconds 129600 --serial-number $ARN_OF_MFA --token-code $TOKEN_CODE" && \
-     	  CREDS=`aws sts get-session-token --duration-seconds 129600 --serial-number $ARN_OF_MFA --token-code $TOKEN_CODE | jq -c`  \
-    	  || { return 1 }}
+  # make sure two arguments are passed else die
+  [ $# -eq 0 ] && { echo "Usage: $0 token-code [profile]" ; return 1 }
+  [ $# -gt 3 ] && { echo "Usage: $0 token-code [profile]" ; return 1 }
 
-     [ $# -eq 2 ] && { \
-        ARN_OF_MFA=`aws configure get aws_mfa_device --profile $2` && \
-        TOKEN_CODE=$1 && \
-     	  echo "Running: aws sts get-session-token --duration-seconds 129600 --serial-number $ARN_OF_MFA --token-code $TOKEN_CODE" && \
-     	  CREDS=`aws sts get-session-token --duration-seconds 129600 --serial-number $ARN_OF_MFA --profile $2 --token-code $TOKEN_CODE | jq -c`  \
-        || { return 1 }}
-    
-     # Write credentials into the cache file
-     echo "New AWS credentials generated:"
-     echo "export AWS_ACCESS_KEY_ID=`echo $CREDS | jq -r .Credentials.AccessKeyId`" | tee ${AWS_CREDS_CACHE}
-     echo "export AWS_SECRET_ACCESS_KEY=`echo $CREDS | jq -r .Credentials.SecretAccessKey`"  | tee -a ${AWS_CREDS_CACHE}
-     echo "export AWS_SESSION_TOKEN=`echo $CREDS | jq -r .Credentials.SessionToken`" | tee -a ${AWS_CREDS_CACHE}
-     echo "export AWS_CREDS_JSON=`echo \'$CREDS\'`"  >> ${AWS_CREDS_CACHE}
-     . "${AWS_CREDS_CACHE}" > /dev/null
-     echo "Expiring: `echo ${AWS_CREDS_JSON} | jq -r .Credentials.Expiration`"
+  [ $# -eq 1 ] && { \
+    ARN_OF_MFA=`${AWS_CLI} configure get aws_mfa_device --profile default` && \
+    TOKEN_CODE=$1 && \
+    echo "Running: ${AWS_CLI} sts get-session-token --duration-seconds ${DURATION_SECONDS} --serial-number ${ARN_OF_MFA} --token-code ${TOKEN_CODE}" && \
+    CREDS=`${AWS_CLI} sts get-session-token --duration-seconds ${DURATION_SECONDS} --serial-number ${ARN_OF_MFA} --token-code ${TOKEN_CODE} | jq -c`  \
+    || { return 1 }}
+
+  [ $# -eq 2 ] && { \
+    ARN_OF_MFA=`${AWS_CLI} configure get aws_mfa_device --profile $2` && \
+    TOKEN_CODE=$1 && \
+    echo "Running: ${AWS_CLI} sts get-session-token --duration-seconds ${DURATION_SECONDS} --serial-number $ARN_OF_MFA --token-code $TOKEN_CODE" && \
+    CREDS=`${AWS_CLI} sts get-session-token --duration-seconds ${DURATION_SECONDS} --serial-number $ARN_OF_MFA --profile $2 --token-code $TOKEN_CODE | jq -c`  \
+    || { return 1 }}
+
+  # Write credentials into the cache file
+  echo "New AWS credentials generated:"
+  echo "export AWS_ACCESS_KEY_ID=`echo $CREDS | jq -r .Credentials.AccessKeyId`" | tee ${AWS_CREDS_CACHE}
+  echo "export AWS_SECRET_ACCESS_KEY=`echo $CREDS | jq -r .Credentials.SecretAccessKey`"  | tee -a ${AWS_CREDS_CACHE}
+  echo "export AWS_SESSION_TOKEN=`echo $CREDS | jq -r .Credentials.SessionToken`" | tee -a ${AWS_CREDS_CACHE}
+  echo "export AWS_CREDS_JSON=`echo \'$CREDS\'`"  >> ${AWS_CREDS_CACHE}
+  . "${AWS_CREDS_CACHE}" > /dev/null
+  echo "Expiring: `echo ${AWS_CREDS_JSON} | jq -r .Credentials.Expiration`"
 }
